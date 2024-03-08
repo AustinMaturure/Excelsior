bd7f44dd1ab2bfaf9054456bf175066df3b0eeb2
Austin <austinematurure@gmail.com>
2024-03-08 18:18:44 +0200

    added style updates

--- frontend/src/footer.jsx
+++ frontend/src/footer.jsx
@@ -9,6 +9,20 @@ export default function Footer() {
             <section className="logo-footer">
                 <h1>Excelsior</h1>
             </section>
+            <div className="map">
+  <iframe
+    title="Google Map"
+    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14219.326960029346!2d30.8050949!3d-27.0038719!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1eeee35b32a52019%3A0x11759755b5983635!2sExcelsior%20News!5e0!3m2!1sen!2sza!4v1709914531250!5m2!1sen!2sza" 
+    width="400"
+  
+    height="200"
+    className="map-frame"
+    style={{ border: '2px solid #213547', borderRadius: "22px" }}
+    allowFullScreen=""
+    loading="async"  
+    referrerPolicy="no-referrer-when-downgrade"
+  ></iframe>
+</div>
             <div className="about-us">
                 <h1>About us</h1>
                 <p className="about-us-text">The <span style={{ color: '#ca212a'}}>Only</span> Registered, Recognised and Approved Local Newspaper Servicing Piet Rietief, Mkhondo and 
