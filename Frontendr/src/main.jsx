8eca8801fd6dbf125388e47428140b0ada0c7f4e
Austin <austinematurure@gmail.com>
2025-02-13 15:14:59 +0200

    feat: Style adjustments to links by adding underline when hovered and changing color

--- Frontendr/src/main.jsx
+++ Frontendr/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/footer.jsx";
+import Footer from "./components/Footer.jsx";
 import "./index.css";
 import App from "./App.jsx";
 import ArticlePage from "./components/ArticlePage.jsx";
